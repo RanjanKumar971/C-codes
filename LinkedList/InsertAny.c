4c8888d7f03f65aaae17e3edfff712d4efab20d4
Ranjan Kumar <ranjankumar1210rk@gmail.com>
2024-12-25 19:57:45 +0530

    Insertion for linkedlist

---
+++ LinkedList/InsertAny.c
@@ -0,0 +1,74 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct Node{
+    int data;
+    struct Node *next;
+};
+
+struct Node * insertAny(struct Node *head,int data,int index){
+    struct Node * n=(struct Node *)malloc(sizeof(struct Node));
+    n->data=data;
+    if(index==0){
+        n->next=head;
+        return n;
+    }
+    else{
+        struct Node *p=head;
+        for(int i=0;i<=index;i++){
+            if(p==NULL){
+                printf("index out of range");
+                return head;
+            }
+            else if (i==index-1){
+                n->next=p->next;
+                p->next=n;
+            }
+            p=p->next;  
+        }
+    }
+    return head;  
+}
+void print_List(struct Node * ptr){
+    while(ptr!=NULL){
+        printf("%d -> ",ptr->data);
+        ptr=ptr->next;
+    }
+    printf("NULL\n");
+}
+int main(){
+    struct Node *head=(struct Node *)malloc(sizeof(struct Node));
+    struct Node *n1=(struct Node *)malloc(sizeof(struct Node));
+    struct Node *n2=(struct Node *)malloc(sizeof(struct Node));
+    struct Node *n3=(struct Node *)malloc(sizeof(struct Node));
+    struct Node *n4=(struct Node *)malloc(sizeof(struct Node));
+
+    head->data=1;
+    head->next=n1;
+
+    n1->data=2;
+    n1->next=n2;
+
+    n2->data=3;
+    n2->next=n3;
+
+    n3->data=4;
+    n3->next=n4;
+
+    n4->data=5;
+    n4->next=NULL;
+
+    print_List(head);
+
+    //insert at any location
+    head=insertAny(head,0,0);
+    print_List(head);
+
+    head=insertAny(head,10,3);
+    print_List(head);
+
+    head=insertAny(head,6,7);
+    print_List(head);
+
+    return 0;
+
+}
---
+++ LinkedList/Insertion.c
@@ -0,0 +1,90 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+void printlist(struct Node *head)
+{
+    while (head != NULL)
+    {
+        printf("%d -> ", head->data);
+        head = head->next;
+    }
+    printf("NULL\n");
+}
+
+// insert at beginnig
+struct Node *insertBeg(struct Node *head, int d)
+{
+    struct Node *n = (struct Node *)malloc(sizeof(struct Node));
+    n->data = d;
+    n->next = head;
+    return n;
+}
+// insert at index
+void insertAt(struct Node *head, int data, int index)
+{
+    struct Node *n = (struct Node *)malloc(sizeof(struct Node));
+    n->data = data;
+    struct Node *p = head;
+    for (int i = 0; i <= index; i++)
+    {
+        if (p == NULL)
+        {
+            printf("Index out of range.\n");
+            return;
+        }
+        if (i == index - 1)
+        {
+            n->next = p->next;
+            p->next = n;
+        }
+        p = p->next;
+    }
+}
+// insert at end
+void insertEnd(struct Node *head, int data)
+{
+    struct Node *n = (struct Node *)malloc(sizeof(struct Node));
+    n->data = data;
+    struct Node *p = head;
+    while (p->next != NULL)
+        p = p->next;
+    p->next = n;
+    n->next = NULL;
+}
+int main()
+{
+    struct Node *head = (struct Node *)malloc(sizeof(struct Node));
+    struct Node *n1 = (struct Node *)malloc(sizeof(struct Node));
+    struct Node *n2 = (struct Node *)malloc(sizeof(struct Node));
+    struct Node *tail = (struct Node *)malloc(sizeof(struct Node));
+
+    head->data = 1;
+    head->next = n1;
+
+    n1->data = 2;
+    n1->next = n2;
+
+    n2->data = 3;
+    n2->next = tail;
+
+    tail->data = 4;
+    tail->next = NULL;
+
+    printlist(head);
+
+    head = insertBeg(head, 0);
+    printlist(head);
+
+    // can't insert at index 0
+    insertAt(head, 100, 5);
+    printlist(head);
+    // printf("%d",head->data);
+    insertEnd(head, 101);
+    printlist(head);
+
+    return 0;
+}
